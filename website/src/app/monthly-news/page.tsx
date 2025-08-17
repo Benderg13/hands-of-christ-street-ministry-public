f1bb5eb05085ff29eab5438529ea0e1fd0efb87d
Grant Bender <grant.bender13@gmail.com>
2025-08-17 17:37:08 -0400

    renamed next.js file structure

--- website/src/app/monthly-news/page.tsx
+++ website/src/app/monthly-news/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
         <main>
             <div className='min-h-screen flex items-center justify-center '>
                 <div className="bg-navy-blue p-8 rounded-lg max-w-xl w-full"> 
-                <h1 className="text-4xl font-bold mb-6 text-center ">Mission Statement</h1>
+                <h1 className="text-4xl font-bold mb-6 text-center ">Monthly News</h1>
             </div>
             </div>
         </main>
